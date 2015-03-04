e0e3eba3878ebeadd2fceb21ef486e8facd740c7
oscar garcia garcia <tartanuk@MacBook-Air-de-oscar.local>
2015-03-04 12:48:39 +0100

    Ejercicio propio Oscar Garcia

---
+++ POscar/POscar/MainWindow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnSUMAClicked (object sender, EventArgs e)
+	{
+		texto.Buffer.Text = nom1.Text+" "+nom2.Text;
+	}
+
+	protected void OnUnirClicked (object sender, EventArgs e)
+	{
+		texto.Buffer.Text = nom3.Text+" "+nom4.Text;
+	}
+}
